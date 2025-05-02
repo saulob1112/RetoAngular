d7916ebe60ff2ff1dc9373e9e91fb1fb72df63a5
saulob <saul.ob1112@gmail.com>
2025-05-02 08:47:26 -0600

    Funcionalidad para editar producto

--- src/app/editar-producto/editar-producto.component.ts
+++ src/app/editar-producto/editar-producto.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Producto } from '../producto';
+import { ProductoService } from '../producto.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-editar-producto',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './editar-producto.component.html'
 })
 export class EditarProductoComponent {
+  producto: Producto = new Producto();
+  id!: number;
+
+  private productoServicio = inject(ProductoService);
+  private ruta = inject(ActivatedRoute);
+  private enrutador = inject(Router);
+
+  ngOnInit() {
+    this.id = this.ruta.snapshot.params['id'];
+    this.productoServicio.obtenerProductoPorId(this.id).subscribe({
+      next: (datos) => this.producto = datos,
+      error: (errores: any) => console.log(errores)
+    });
+  }
+
+  onSubmit() {
+    this.guardarProducto();
+  }
+
+  guardarProducto() {
+    this.productoServicio.editarProducto(this.id, this.producto).subscribe({
+      next: (datos) => this.irProductoLista(),
+      error: (errores: any) => console.log(errores)
+    });
+  }
+
+  irProductoLista() {
+    this.enrutador.navigate(['/productos']);
+  }
 
 }
--- src/app/producto-lista/producto-lista.component.ts
+++ src/app/producto-lista/producto-lista.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { Producto } from '../producto';
 import { ProductoService } from '../producto.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-producto-lista',
@@ -11,6 +12,7 @@ export class ProductoListaComponent {
   productos!: Producto[];
 
   private productoServicio = inject(ProductoService);
+  private enrutador = inject(Router);
 
   ngOnInit() {
     this.obtenerProductos();
@@ -29,4 +31,7 @@ export class ProductoListaComponent {
     )
   }
 
+  editarProducto(id: number) {
+    this.enrutador.navigate(['editar-producto', id]);
+  }
 }
